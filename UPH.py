11f593a7c012018dcc1d9f5417d3e0daa09f94aa
Justin Garza <jlgarza10@gmail.com>
2018-12-26 16:49:29 -0600

    Automated the plotting, using tabula and openCSV libraries to automatically gather and parse data. Does not handle wrong input from user yet, will crash.

--- UPH.py
+++ UPH.py
@@ -1,57 +1,84 @@
+import os
 import matplotlib.pyplot as plt
-import PyPDF2
-from tabula import read_pdf
-import numpy as np
 import csv
-import pandas
+import pandas as pd
+import glob
+import numpy as np
+# df = pandas.read_csv('C:\\Users\\jlgar\\Desktop\\UPH_Data\\CSVFiles\\tabula-FASTweb-09-17_09-24.csv')
+# print(df)
+#
+#
+# print(df.dtypes)
+# print(df.describe())
+#
+# df['UPH'] = df['UPH'].map(lambda x: x.rstrip('/ hour'))
+# df['IPH'] = df['IPH'].map(lambda x: x.rstrip('/ hour'))
+#
+# df['UPH'] = df['UPH'].astype(float)
+# df['IPH'] = df['IPH'].astype(float)
 
-x = ["09-24", "10-01", "10-08", "10-15"]
 
-b374813 = [106.3, 133.3, 127.4, 122.4]
-b399617 = [132.9, 145.6, 132.3, 138.1]
-b575129 = [143.1, 153.0, 153.9, 161.1]
-f199327 = [184.1, 204.3, 157.0, 170.8]
-g444698 = [115.4, 119.0, 135.4, 117.0]
-g585396 = [115.0, None, 94.3, 143.7]
-i411561 = [106.8, 102.9, 118.2, 121.5]
+path = r'C:\Users\jlgar\Desktop\UPH_Data\CSVfiles'
+allFiles = glob.glob(os.path.join(path, "*.csv", ))
 
-plt.plot(x, b374813, label="b374813")
-plt.plot(x, b399617, label="b399617")
-plt.plot(x, b575129, label="b575129")
-plt.plot(x, f199327, label="f199327")
-plt.plot(x, g444698, label="g444698")
-plt.plot(x, g585396, label="g585396")
-plt.plot(x, i411561, label="i411561")
+concatDF = (pd.read_csv(f) for f in allFiles)
+concatDF = pd.concat(concatDF, ignore_index=True)
 
-plt.xlabel('Week Ending')
-plt.ylabel('UPH')
+concatDF['UPH'] = concatDF['UPH'].map(lambda x: x.rstrip('/ hour'))
+concatDF['IPH'] = concatDF['IPH'].map(lambda x: x.rstrip('/ hour'))
+concatDF['UPH'] = concatDF['UPH'].astype(float)
+concatDF['IPH'] = concatDF['IPH'].astype(float)
+WKE = ["9-24", "10-01", "10-08", "10-15"]
+
+userData = concatDF[['User', 'UPH']]
+
+mydict = {}
+for index in range(len(userData)):
+    currentid = userData.iloc[index, 0]
+    currentvalue = userData.iloc[index, 1]
+    mydict.setdefault(currentid, [])
+    mydict[currentid].append(currentvalue)
+
+myDF = pd.DataFrame()
+myDF = pd.DataFrame.from_dict(mydict, orient='index', columns=WKE)
+print(myDF)
+
+
+print("Enter a user ID from the above list to see graph\nOr enter quit to plot everybody.")
+choice = input()
+while(choice != "quit"):
+    print("Heres " + choice + " you selected.\n")
+    plt.plot(WKE, myDF.loc[choice])
+    plt.ylabel("Units Per Hour")
+    plt.xlabel("Weekending")
+    plt.legend()
+    plt.show()
+    print(myDF)
+    choice = input("Enter a user ID from the above list to see graph\nOr enter quit to plot everybody.")
+
+for idx in range(len(myDF)):
+    plt.plot(WKE, myDF.iloc[idx])
 
 plt.legend()
-#plt.show()
-# plt.show()
+plt.ylabel("Units Per Hour")
+plt.xlabel("Weekending")
 
-# df = read_pdf('C://Users//jlgar//Desktop//UPH_Data//FASTweb-09-17_09-24.pdf')
-
-"""x = []
-y = []
-
-with open('C:/Users/jlgar/Desktop/UPH_Data/tabula-FASTweb-09-17_09-24.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        x.append(str(row[0]))
-        y.append(float(row[1]))
-
-plt.plot(x, y)
-plt.show()"""
-df = pandas.read_csv('C:/Users/jlgar/Desktop/UPH_Data/tabula-FASTweb-09-17_09-24.csv')
-print(df)
+plt.show()
 
 
 
-
-
-
-
+#print(mydict)
+# data = {"x": [], "y": [], "label": []}
+# for label, coord in mydict.items():
+#     if len(coord) < 4:
+#         for i in range(len(coord)):
+#             data["x"].append(WKE[i])
+#     else:
+#         data["x"].append(WKE)
+#     data["y"].append(coord)
+#     data["label"].append(label)
+#
+# print(data)
 
 
 
